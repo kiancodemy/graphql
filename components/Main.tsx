df6d5143f13e6954aedeb70c5ec2dc891f9c776f
kian kheirani <kiankheirani@gmail.com>
2024-07-30 19:27:26 +0330

    '34'

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./Header";
-import Content from "./Content";
+import MyContent from "./MyContent";
 import History from "./History";
 
 export default function Main() {
   return (
     <div className="container mx-auto lg:max-w-7xl md:max-w-4xl max-w-[340px] min-h-[100vh]">
       <Header></Header>
-      <Content></Content>
+      <MyContent></MyContent>
       <History></History>
     </div>
   );
